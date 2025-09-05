3e4403851879dd1ffdf662049fc2ffca5a8b44f5
liupenghui <liupenghui@58.com>
2025-09-05 15:21:28 +0800

    add serve

--- src/esm/index.mjs
+++ src/esm/index.mjs
@@ -1,3 +1,8 @@
 // ESM 模块入口文件
-import  {m} from './module.mjs'
-console.log(m)
+// import  {m} from './module.mjs'
+// console.log(m)
+
+import * as m from './module.mjs'
+console.log(m.bar)
+console.log(m.foo())
+console.log(m.m)
--- src/esm/module.mjs
+++ src/esm/module.mjs
@@ -3,3 +3,9 @@
 export const m = {
   foo: 1,
 }
+
+export function foo() {
+  console.log('Hello World from foo.')
+}
+
+export const bar = 'Hello World from bar.'
