b7e69ce3a17292d21039ed6a0e89fead39f5cda6
liupenghui <liupenghui@58.com>
2025-09-05 15:50:57 +0800

    引入md5

--- src/esm/index.mjs
+++ src/esm/index.mjs
@@ -1,5 +1,8 @@
 // ESM 模块入口文件 - 展示多种导入方式
 
+import md5 from 'md5'
+console.log(' hello world md5加密后:', md5('Hello World'))
+
 // 1. 导入默认导出
 import m from './module.mjs'
 console.log('默认导出:', m)
@@ -13,17 +16,6 @@ console.log('命名导出 bar:', bar)
 import * as moduleExports from './module.mjs'
 console.log('所有导出:', moduleExports)
 
-// 4. 导入工具函数模块
-import { formatDate, capitalize, VERSION } from './utils.mjs'
-console.log('工具函数测试:')
-console.log('格式化日期:', formatDate(new Date()))
-console.log('首字母大写:', capitalize('hello world'))
-console.log('版本号:', VERSION)
-
-// 5. 导入常量模块
-import { API_BASE_URL, COLORS } from './constants.mjs'
-console.log('API 地址:', API_BASE_URL)
-console.log('主题颜色:', COLORS)
 
 // 6. 混合导入（从同一个模块导入默认和命名导出）
 import defaultExport, { foo as renamedFoo } from './module.mjs'
